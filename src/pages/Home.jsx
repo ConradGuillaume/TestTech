5251562310b8dfea62d3d51bf7f7fb16b0116bee
ConradGuillaume <conrad.guillaume0501@gmail.com>
2023-03-02 09:45:01 +0100

    setup ready

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProgressBar from "../Component/ProgressBar";
+
+const Home = () => {
+  return (
+    <div>
+      test
+      <ProgressBar />
+    </div>
+  );
+};
+
+export default Home;
